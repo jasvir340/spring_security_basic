0fce844000322db4d01b8e7b4cf76501c3f3e5a6
jasvi <jasvirdhillon340@gmail.com>
2021-08-13 10:32:34 +0530

    In Memory Authentication

--- springsecuritysection2/src/main/java/com/topnotch/springsecuritysection2/config/ProjectSecurityConfig.java
+++ springsecuritysection2/src/main/java/com/topnotch/springsecuritysection2/config/ProjectSecurityConfig.java
@@ -1,15 +1,16 @@
 package com.topnotch.springsecuritysection2.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
 		
 		  http .authorizeRequests()
 		  .antMatchers("/myAccount").authenticated()
@@ -18,8 +19,20 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 		  .antMatchers("/myCards").authenticated()
 		  .antMatchers("/contact","/notices").permitAll() .and() .formLogin() .and()
 		  .httpBasic();
-		 
 			
 	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+		.inMemoryAuthentication()
+			.withUser("admin").password("1234").authorities("admin")
+		.and()
+			.withUser("user").password("1234").authorities("read")
+		.and()
+			.passwordEncoder(NoOpPasswordEncoder.getInstance());
+	}
+	
+	
 
 }
