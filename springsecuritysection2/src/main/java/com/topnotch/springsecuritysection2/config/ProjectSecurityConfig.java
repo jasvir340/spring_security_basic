3c0385633625a228621d9417b6b136b3e0402e86
jasvi <jasvirdhillon340@gmail.com>
2021-08-15 01:16:16 +0530

    Testing IntelliJ 3

--- springsecuritysection2/src/main/java/com/topnotch/springsecuritysection2/config/ProjectSecurityConfig.java
+++ springsecuritysection2/src/main/java/com/topnotch/springsecuritysection2/config/ProjectSecurityConfig.java
@@ -1,10 +1,13 @@
 package com.topnotch.springsecuritysection2.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -25,15 +28,9 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 			
 	}
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-		.inMemoryAuthentication()
-			.withUser("admin").password("1234").authorities("admin")
-		.and()
-			.withUser("user").password("1234").authorities("read")
-		.and()
-			.passwordEncoder(NoOpPasswordEncoder.getInstance());
+	@Bean
+	public UserDetailsService userDetailsService(DataSource dataSource){
+		return new JdbcUserDetailsManager(dataSource);
 	}
 
 }
