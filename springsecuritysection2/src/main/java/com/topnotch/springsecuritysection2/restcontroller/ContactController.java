88c45c69d7388962405c8f0e495c168366d74aa3
jasvi <jasvirdhillon340@gmail.com>
2021-08-17 04:17:49 +0530

    No changes

--- springsecuritysection2/src/main/java/com/topnotch/springsecuritysection2/restcontroller/ContactController.java
+++ springsecuritysection2/src/main/java/com/topnotch/springsecuritysection2/restcontroller/ContactController.java
@@ -3,7 +3,6 @@ package com.topnotch.springsecuritysection2.restcontroller;
 import com.topnotch.springsecuritysection2.model.Contact;
 import com.topnotch.springsecuritysection2.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
