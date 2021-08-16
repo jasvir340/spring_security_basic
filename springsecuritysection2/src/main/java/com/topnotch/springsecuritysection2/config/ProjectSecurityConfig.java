8d9947814c74df8a0e310d92048369fa647c9954
jasvi <jasvirdhillon340@gmail.com>
2021-08-17 02:04:27 +0530

    JSON Web Token Support Added

--- springsecuritysection2/src/main/java/com/topnotch/springsecuritysection2/config/ProjectSecurityConfig.java
+++ springsecuritysection2/src/main/java/com/topnotch/springsecuritysection2/config/ProjectSecurityConfig.java
@@ -1,18 +1,17 @@
 package com.topnotch.springsecuritysection2.config;
 
-import com.topnotch.springsecuritysection2.filter.AuthoritiesLoggingAfterFilter;
-import com.topnotch.springsecuritysection2.filter.AuthoritiesLoggingAtFilter;
-import com.topnotch.springsecuritysection2.filter.RequestValidationBeforeFilter;
+import com.topnotch.springsecuritysection2.filter.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
@@ -21,24 +20,29 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.cors().configurationSource(request -> {
+        http
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .cors().configurationSource(request -> {
                     CorsConfiguration corsConfiguration = new CorsConfiguration();
                     corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
                     corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
                     corsConfiguration.setAllowCredentials(true);
+                    corsConfiguration.setExposedHeaders(Arrays.asList("Authorization"));
                     corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
                     corsConfiguration.setMaxAge(3600L);
                     return corsConfiguration;
                 })
                 .and()
-                .csrf().ignoringAntMatchers("/contact").csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .and()
+                .csrf().disable()
                 .addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class)
                 .addFilterAfter(new AuthoritiesLoggingAfterFilter(), BasicAuthenticationFilter.class)
+                .addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class)
+                .addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
                 .addFilterAt(new AuthoritiesLoggingAtFilter(), BasicAuthenticationFilter.class)
                 .authorizeRequests()
                 .antMatchers("/myAccount").hasRole("USER")
-                .antMatchers("/myBalance").hasAnyRole("ADMIN","USER")
+                .antMatchers("/myBalance").hasAnyRole("ADMIN", "USER")
                 .antMatchers("/myLoans").hasRole("ROOT")
                 .antMatchers("/myCards").authenticated()
                 .antMatchers("/contact", "/notices").permitAll()
---
+++ springsecuritysection2/src/main/java/com/topnotch/springsecuritysection2/constants/SecurityConstants.java
@@ -0,0 +1,6 @@
+package com.topnotch.springsecuritysection2.constants;
+
+public class SecurityConstants {
+    public static final String JWT_KEY = "jxgEQeXHuPq8VdbyYFNkANdudQ53YUn4";
+    public static final String JWT_HEADER = "Authorization";
+}
